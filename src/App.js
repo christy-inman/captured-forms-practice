a2044128182a96726571224e37de88df444c3337
github username <github email address>
2019-08-12 11:39:34 -0600

    form and css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import PlantList from './components/PlantList'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    plants: [{
+      name: "Fern"
+    },{
+      name: "Cactus"
+    },{
+      name: "Devil's Ivy"
+    }],
+    plant: "",
+  }
+  handleChange = event => {
+    this.setState({
+      plant: event.target.value
+    })
+  }
+  handleSubmit = event => {
+    event.preventDefault()
+    this.setState({
+      plants: [...this.state.plants, {name: this.state.plant}],
+      plant: "",
+    })
+    
+  }
+  render() {
+    return (
+      <div>
+        <h1>Plants</h1>
+        <PlantList plants={this.state.plants} />
+          <form onSubmit={this.handleSubmit}>
+            <input 
+              type="text" 
+              placeholder="Enter a plant" 
+              value={this.state.plant} 
+              onChange={this.handleChange} 
+            />
+            <input type="submit" value="Add" />
+          </form>
+      </div>
+    )
+  }
 }
 
 export default App;
